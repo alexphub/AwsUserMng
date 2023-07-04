fa6774341ad1d1d63ce9e910e0821a21c9260514
Alex Polischuk <alexpol@walla.com>
2023-07-04 09:09:38 +0300

    fix response json

--- src/exAddUsersBulk/index.js
+++ src/exAddUsersBulk/index.js
@@ -19,7 +19,7 @@ exports.handler = async (event) => {
     
     //console.info("line length" + lines.length);
     
-    let users_id ="{ ";
+    let users_id ="{{";
     for (let i = 0; i <lines.length; i++) {
         
       let user = lines[i].split(',');
@@ -35,14 +35,14 @@ exports.handler = async (event) => {
       let userId = await saveUserToDynamoDB(user);
       console.info("user[" + i + "]" + user[1] + " " + user[2] + " id: " + userId );
       
-      users_id += "user: " + user[1] + ", id: " + userId;
+      users_id += "user: " + user[1] + ", id: " + userId + "}";
       
       if (i < lines.length - 1) {
-        users_id += ", ";
+        users_id += ", {";
       }
     }
     
-    users_id += " }";
+    users_id += "}";
     
     let msg = JSON.stringify(users_id);
     
